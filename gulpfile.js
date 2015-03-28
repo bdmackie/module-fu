b7f7635895eb730d9b483fe5f468a0f764b14340
Ben Mackie <bdmackie@gmail.com>
2015-03-29 02:53:41 +1100

    fixed package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var validate = require('gulp-nice-package');
 
 gulp.task('test', [], function () {
     gulp.src('test/**/test*.js', {read: false})
         .pipe(mocha({reporter: 'spec', recursive: true}));
+});
+
+gulp.task('validate-json', function () {
+  return gulp.src('package.json')
+    .pipe(validate());
 });
